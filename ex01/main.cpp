4f1d4e53890933761a8a4fd8affe9a4280ecea9b
Mikko Suokas <msuokas@c2r1p2.hive.fi>
2025-09-03 16:14:04 +0300

    new updates

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,16 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msuokas <msuokas@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/03 13:58:10 by msuokas           #+#    #+#             */
+/*   Updated: 2025/09/03 13:58:11 by msuokas          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "Fixed.hpp"
 
 int	main( void ) {
 	Fixed a;
 	Fixed const b( 10 ); Fixed const c( 42.42f ); Fixed const d( b );
+	Fixed const e(5.12f);
+	Fixed const f ( e );
 	a = Fixed( 1234.4321f );
 	std::cout << "a is " << a << std::endl;
 	std::cout << "b is " << b << std::endl;
 	std::cout << "c is " << c << std::endl;
 	std::cout << "d is " << d << std::endl;
+
 	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
 	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
 	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
 	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+
+	std::cout << "a is " << a.toFloat() << " as float" << std::endl;
+	std::cout << "e is " << e.toFloat() << " as float" << std::endl;
+	std::cout << "c is " << c.toFloat() << " as float" << std::endl;
+	std::cout << "f is " << f.toFloat() << " as float" << std::endl;
 	return 0;
 }
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,20 +6,18 @@
 /*   By: msuokas <msuokas@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/16 14:22:42 by msuokas           #+#    #+#             */
-/*   Updated: 2025/09/02 16:07:03 by msuokas          ###   ########.fr       */
+/*   Updated: 2025/09/03 14:19:11 by msuokas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 
 int main() {
-	//std::cout << "Constructor tests: \n";
 	Fixed a;
 	Fixed b(10);
 	Fixed c(42.42f);
 	Fixed d(b);
 
-	//std::cout << "Assignment test: \n";
 	a = Fixed(123.456f);
 
 	std::cout << "Values to test with: \n";
@@ -31,16 +29,16 @@ int main() {
 	std::cout << "Arithmetic operations: \n";
 	Fixed sum = b + c;
 	Fixed sum_of_three = b + b + b;
-	Fixed diff = c - b;
-	Fixed prod = b * c;
-	Fixed quot = c / b;
+	Fixed subtraction = c - b;
+	Fixed multiplication = b * c;
+	Fixed dividend = c / b;
 
 	std::cout << "b + c = " << sum << "\n";
 	std::cout << "b + b + b = " << sum_of_three << "\n";
 
-	std::cout << "c - b = " << diff << "\n";
-	std::cout << "b * c = " << prod << "\n";
-	std::cout << "c / b = " << quot << "\n";
+	std::cout << "c - b = " << subtraction << "\n";
+	std::cout << "b * c = " << multiplication << "\n";
+	std::cout << "c / b = " << dividend << "\n";
 	std::cout << "-----------------------\n\n";
 	std::cout << "Comparisons:\n";
 
@@ -73,14 +71,18 @@ int main() {
 	Fixed high(3.3f);
 	std::cout << "low = 3.3, high = 7.7: " << std::endl;
 	std::cout << "Max of 'low' and 'high': " << Fixed::max(low, high) << "\n";
-	std::cout << "Max of const 'low' and 'high': " << Fixed::max(static_cast<const Fixed&>(low), static_cast<const Fixed&>(high)) << "\n\n";
+	const Fixed const_low(7.7f);
+	const Fixed const_high(3.3f);
+	std::cout << "Max of const 'low' and const 'high': " << Fixed::max(const_low, const_high) << "\n\n";
 
 	std::cout << "Min:\n";
 	Fixed low2(1);
 	Fixed high2(10);
 	std::cout << "low = 1, high = 10: " << std::endl;
 	std::cout << "Min of 'low' and 'high' " << Fixed::min(low2, high2) << "\n";
-	std::cout << "Min of const 'low' and const 'high' " << Fixed::min(static_cast<const Fixed&>(low2), static_cast<const Fixed&>(high2)) << "\n";
+	const Fixed const_low2(1);
+	const Fixed const_high2(10);
+	std::cout << "Min of const 'low' and const 'high': " << Fixed::min(const_low2, const_high2) << "\n\n";
 
 	std::cout << "-----------------------\n\n";
 	std::cout << "From fixed point to int conversion:\n";
